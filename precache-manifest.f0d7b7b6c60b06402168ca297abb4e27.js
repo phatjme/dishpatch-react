2a8ace6fed975c497aa3c7268d19691876a77f79
Joseph Hayden <josephphayden@gmail.com>
2020-04-02 12:56:29 +0100

    Updates

---
+++ precache-manifest.f0d7b7b6c60b06402168ca297abb4e27.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19876de6db8da83665fdcfa4d54a36f",
+    "url": "/dishpatch-react/index.html"
+  },
+  {
+    "revision": "753bacea0027ce7e224c",
+    "url": "/dishpatch-react/static/css/main.2674c664.chunk.css"
+  },
+  {
+    "revision": "011c2ea75b4b5f98198c",
+    "url": "/dishpatch-react/static/js/2.0cc11f47.chunk.js"
+  },
+  {
+    "revision": "17dffe90612e048fd9b4a769ac11b9cb",
+    "url": "/dishpatch-react/static/js/2.0cc11f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753bacea0027ce7e224c",
+    "url": "/dishpatch-react/static/js/main.ce00cf07.chunk.js"
+  },
+  {
+    "revision": "8e9e893bfb808ca5828b",
+    "url": "/dishpatch-react/static/js/runtime-main.a2a443c2.js"
+  },
+  {
+    "revision": "4a2f2ea45a0bb1abe81b47d0afde4aae",
+    "url": "/dishpatch-react/static/media/FredokaOne-Regular.4a2f2ea4.ttf"
+  },
+  {
+    "revision": "005454d8f0c400b618dc3970484b8365",
+    "url": "/dishpatch-react/static/media/Nunito-Black.005454d8.ttf"
+  },
+  {
+    "revision": "dba92cb9dc60f9f35cbf62428afd3ac1",
+    "url": "/dishpatch-react/static/media/Nunito-Regular.dba92cb9.ttf"
+  }
+]);
