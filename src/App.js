8320ca2a92916646133300dbf9f73b8b4f55de79
Joseph Hayden <josephphayden@gmail.com>
2020-04-02 10:38:33 +0100

    Load suppliers from GSheet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Tabletop from "tabletop";
 
-const App = () => (
-  <div>
-    <Header />
-  </div>
-);
+const sheetUrl =
+  "https://docs.google.com/spreadsheets/d/1nVWGi-4mlCqQeCJ6S2yCRrOqWbSYgAW0fkkfcSx5z8U/pubhtml";
+
+const App = () => {
+  const [suppliers, setSuppliers] = useState([]);
+
+  useEffect(() => {
+    Tabletop.init({
+      key: sheetUrl,
+      simpleSheet: true
+    })
+      .then(setSuppliers)
+      .catch(err => console.error("Error loading suppliers", err));
+  }, []);
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
 
 export default App;
